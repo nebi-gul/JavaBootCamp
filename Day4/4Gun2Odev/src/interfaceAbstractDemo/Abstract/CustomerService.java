3549fe02340c301b46baffa3e6e5aa744086cb8c
Nebi GUL <63506163+nebi-gul@users.noreply.github.com>
2022-03-19 23:07:04 +0300

    Delete Day4/4Gun2Odev/src directory

--- Day4/4Gun2Odev/src/interfaceAbstractDemo/Abstract/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package interfaceAbstractDemo.Abstract;
-
-import interfaceAbstractDemo.Entities.Customer;
-
-public interface CustomerService {//CustomerService temel bir interface görevi görecek.Nesnelerin temel bir CustomerService'i vardýr
-	void save(Customer customer) throws Exception;
-}
